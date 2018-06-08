57aa5c57adc4996b3b4ac64dfb7e6980bcefc542
mrrck <123>
2018-06-08 15:51:21 +0800

    init

---
+++ app/src/main/java/com/bobo/dagger2/text_dagger2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bobo.dagger2.text_dagger2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
